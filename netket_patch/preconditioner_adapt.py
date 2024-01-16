4a3188a5ccf94bf7dd6ba2f99f3da7c02c373cb9
Dian Wu <dian.wu@epfl.ch>
2024-01-16 13:30:26 +0100

    Fix protocol

--- netket_patch/preconditioner_adapt.py
+++ netket_patch/preconditioner_adapt.py
@@ -7,7 +7,9 @@ from netket.vqs import VariationalState
 
 
 class LHSConstructorAdaptT(Protocol):
-    def __call__(self, vstate: VariationalState, diag_shift: float) -> LinearOperator:
+    def __call__(
+        self, vstate: VariationalState, *, diag_shift: float
+    ) -> LinearOperator:
         pass
 
 
@@ -20,7 +22,7 @@ class LinearPreconditionerAdapt:
     info: Any = None
 
     def __call__(self, vstate, gradient, step):
-        lhs = self.lhs_constructor(vstate=vstate, diag_shift=vstate.diag_shift)
+        lhs = self.lhs_constructor(vstate, diag_shift=vstate.diag_shift)
         x0 = self.x0 if self.solver_restart else None
         self.x0, self.info = lhs.solve(self.solver, gradient, x0=x0)
         return self.x0
